7d44135be9f5ea4bd960a8f40932ef7634803060
Wayne Durack <wayne.durack@gmail.com>
2017-08-24 22:58:59 +0100

    Added more logging, fixed a bug

--- index.js
+++ index.js
@@ -8,12 +8,26 @@ const CodeshipWrapper = require('./codeship-node-promise-wrapper')
 const Codeship = new CodeshipWrapper({apiKey})
 
 function restartLastBuild (repositoryName, branchName = 'master') {
+  console.log(`Ready to query Codeship for ${repositoryName}:${branchName}`)
   return Codeship.listProjects()
-    .then(projects => projects.find(project => project.repository_name === repositoryName))
-    .then(project => project.builds.filter(build => build.branch === branchName).unshift())
+    .then(projects => {
+      console.log(`${projects.length} project(s) found`)
+      return projects.find(project => project.repository_name === repositoryName)
+    })
+    .then(project => {
+      console.log(`${project.builds.length} build(s) found for ${project.repository_provider}:${project.repository_name}`)
+      console.log(`Filtering builds to target ${branchName}`)
+      return project.builds.filter(build => build.branch === branchName).shift()
+    })
     // TODO: make testing abort more graceful
-    .then(build => !testing ? Codeship.restartBuild(build.id) : Promise.reject('Just testing'))
-    .then(newBuild => (newBuild.status === 'initiated' && !newBuild.finished_at) ? newBuild : Promise.reject(newBuild))
+    .then(build => {
+      console.log(`${build.id} is last build`)
+      return testing ? Promise.reject('Aborted early because of CODESHIP_TESTING flag') : Codeship.restartBuild(build.id)
+    })
+    .then(newBuild => {
+      console.log('New build returned', newBuild)
+      return (newBuild.status === 'initiated' && !newBuild.finished_at) ? newBuild : Promise.reject(newBuild)
+    })
 }
 
 exports.handler = function (event, context, callback) {
